f4687fe1ea4e043eeff7981492c8ec213bc5b30b
ANTON ZAHARIA <zaharia_anton@yahoo.com>
2020-04-22 18:00:56 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -30,11 +30,9 @@ class ApplicationController < Sinatra::Base
 		erb :login
 	end
 
-	post "/login" do
-    post "/login" do
-  user = User.find_by(:username => params[:username])
-end
-	end
+  post "/login" do
+    user = User.find_by(:username => params[:username])
+  end
 
 	get "/success" do
 		if logged_in?
