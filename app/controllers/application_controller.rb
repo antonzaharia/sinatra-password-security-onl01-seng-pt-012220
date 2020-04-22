30fd4c8c405ecce32123f9989adc8d7e960d5a2c
ANTON ZAHARIA <zaharia_anton@yahoo.com>
2020-04-22 18:14:08 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -36,6 +36,7 @@ class ApplicationController < Sinatra::Base
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
       erb :success
+      binding.pry
     else
       redirect "/failure"
     end
